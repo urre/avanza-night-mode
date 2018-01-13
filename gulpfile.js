44c4853a85b6be868249c4d2650ab95d68d35dd2
Urban Sanden <hej@urre.me>
2018-01-13 18:08:25 +0100

    First commit

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+var gulp = require('gulp')
+var zip = require('gulp-zip')
+path = require('path')
+
+gulp.task('zip', function() {
+	return gulp
+		.src(
+			[
+				'./src/icons/*.png',
+				'./src/css/avanza-night-mode.css',
+				'manifest.json'
+			],
+			{
+				base: '.'
+			}
+		)
+		.pipe(zip('avanza-night-mode.zip'))
+		.pipe(gulp.dest('./ship'))
+})
